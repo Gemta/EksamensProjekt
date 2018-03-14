c647a71faf959af70ac6be245f085ab65fc2d328
Gemta <opa_asser@hotmail.com>
2018-03-14 11:20:52 +0100

    Added pin-variables and VB 2010 command-funktion
    Added the variables for the pins, and the framework for accepting commands from Visual Basic 2010 in the loop-fuinktion.

--- StorageSystemControl/StorageSystemControl.ino
+++ StorageSystemControl/StorageSystemControl.ino
@@ -1,20 +1,40 @@
-int pinMotorMoveOut = 13;
-int pinMotorMoveIn = 12;
-int pinMagnetOn = 11;
-int pinMagnetOff = 10;
-int pinItemRetrieved = 9;
+int pin_motor_moveout = 13;
+int pin_motor_movein = 12;
+int pin_motor_up = 11;
+int pin_motor_down = 10;
+int pin_magneton = 9;
+int pin_magnetoff = 8;
+int pin_extend = 7;
+int pin_extract = 6;
+
+String item_retrieve;
 
 void setup() {
-  pinMode(pinMotorMoveOut, OUTPUT);
-  pinMode(pinMotorMoveIn, OUTPUT);
-  pinMode(pinMagnetOn, OUTPUT);
-  pinMode(pinMagnetOff, OUTPUT);
-  pinMode(pinItemRetrieved, INPUT);
+  pinMode(pin_motor_moveout, OUTPUT);
+  pinMode(pin_motor_movein, OUTPUT);
+  pinMode(pin_motor_up, OUTPUT);
+  pinMode(pin_motor_down, OUTPUT);
+  pinMode(pin_magneton, OUTPUT);
+  pinMode(pin_magnetoff, OUTPUT);
+  pinMode(pin_extend, OUTPUT);
+  pinMode(pin_extract, OUTPUT);
 }
 
 void retriveItem(){
-  
-    
+
+  //Moves out to location
+
+
+  //
+  digitalWrite(pinExtend, HIGH);
+  delay(1000);
+  digitalWrite(pinMagnetOn, HIGH);
+  delay(500);
+  digitalWrite(pinExtract, HIGH);
+
+  //Move back to starting area
+
+
 }
 
 void storeItem(){
@@ -24,6 +44,19 @@ void storeItem(){
 
 void loop() {
 
+  if(Serial.available() > 0){
+    auto Data = Serial.readString();
+    if(Data = "store"){
+      storeItem();
+    }
+    else if(Data = item_retrieve){
+      retrieveItem();
+    }
+    else if(Data != "store" || itemRetrieve){
+      Serial.println("Item not found, or command not valid.");
+    }
+  }
+  
 }
 
 
